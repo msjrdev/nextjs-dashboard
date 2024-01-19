f5eda54e764d5964fceae1930f9dc71cc346a661
Manoel Silva Junior <msilvajrdev@gmail.com>
2024-01-19 01:39:31 -0300

    Finalizado o capítulo 7 - Buscando dados

--- .history/app/dashboard/page_20240119013428.tsx
+++ .history/app/dashboard/page_20240119013428.tsx
@@ -21,8 +21,8 @@ export default async function Page() {
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
         <Card
           title="Total Customers"
           value={numberOfCustomers}
